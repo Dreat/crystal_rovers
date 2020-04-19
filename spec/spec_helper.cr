6704c7bb0b773ddea8339725c0e7957aefeb150d
Lukasz Skotarek <l.skotarek@blockfi.com>
2020-04-19 12:23:23 +0200

    working store + tests

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,3 @@
 require "spec"
 require "../src/rovers"
+require "../src/store.cr"
--- src/rovers.cr
+++ src/rovers.cr
@@ -1,19 +1,2 @@
-require "rover"
-class Store
-    def self.rovers()
-        @@rovers || (@@rovers = {} of String => Rover)
-    end
-
-    def self.create(name)
-        @@rovers[name] = Rover.new()
-    end
-
-    def self.delete(name)
-        @@rovers.delete(name)
-    end
-
-    def self.get(name)
-        @@rovers[name]
-    end
-
+class Rovers
 end
